e446ed0639030a11d3a55adfa6354173d7511589
Tung Dao <me@tungdao.com>
2018-07-16 11:56:21 +0700

    block json serialize

--- blockchain.py
+++ blockchain.py
@@ -1,4 +1,40 @@
-from tornado import web
+import json
+from datetime import datetime
 
 
-class Server(web.Application):
+class Block(object):
+    def __init__(self, index, previous_hash, timestamp, data, nonce, hashvalue=''):
+        self.index = index
+        self.previous_hash = previous_hash
+        self.timestamp = timestamp
+        self.data = data
+        self.nonce = nonce
+        self.hash = hashvalue
+
+    def calculate_hash(self):
+        # TODO
+        pass
+
+    @staticmethod
+    def from_previous(block, data):
+        return Block(block.index + 1, block.hash, datetime.now(), data, 0)
+
+    @staticmethod
+    def from_json(block):
+        block = Block(**block)
+        assert block.calculate_hash() == block.hash
+        return block
+
+    def __repr__(self):
+        return str(self.__dict__)
+
+
+GENESIS = Block(
+    0, '', 1522983367254, None, 0,
+    'e063dac549f070b523b0cb724efb1d4f81de67ea790f78419f9527aa3450f64c'
+)
+
+
+class JSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        return o.__dict__
--- main.py
+++ main.py
@@ -1,17 +1,22 @@
-from flask import Flask
-
-# from . import blockchain
+import blockchain
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 
-app.blocks = []
+app.blocks = [blockchain.GENESIS]
 app.peers = {}
+app.json_encoder = blockchain.JSONEncoder
 
 
 @app.route('/blocks', methods=['GET'])
 def list_blocks():
-    return app.blocks
+    return jsonify(app.blocks)
+
+
+@app.route('/peers', methods=['GET'])
+def list_peers():
+    return jsonify(app.peers)
 
 
 if __name__ == '__main__':
